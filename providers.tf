5a809320d8dbe91c0c29113924981d810e109b07
amularczyk <amularczyk@pgs-soft.com>
2019-02-12 11:28:58 +0100

    Adding variables for provider

--- providers.tf
+++ providers.tf
@@ -1,5 +1,17 @@
+variable "subscription_id" {
+}
+variable "tenant_id" {
+}
+variable "client_id" {
+}
+variable "client_secret" {}
+
 provider "azurerm" {
   version = "~> 1.21.0"
+  subscription_id = "${var.subscription_id}"
+  tenant_id = "${var.tenant_id}"
+  client_id = "${var.client_id}"
+  client_secret = "${var.client_secret}"
 }
 
 # terraform {
