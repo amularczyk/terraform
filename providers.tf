e846be4f2a678860b35aa78cca5ceba7cf16427f
amularczyk <amularczyk@pgs-soft.com>
2019-02-12 09:50:17 +0100

    Clearing master branch

--- providers.tf
+++ providers.tf
@@ -2,11 +2,11 @@ provider "azurerm" {
   version = "~> 1.21.0"
 }
 
-terraform {
-  backend "azurerm" {
-    storage_account_name = "terraform8"
-    container_name = "terraform"
-    resource_group_name = "terraform-state"
-    key = "terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     storage_account_name = "terraform8"
+#     container_name = "terraform"
+#     resource_group_name = "terraform-state"
+#     key = "terraform.tfstate"
+#   }
+# }
